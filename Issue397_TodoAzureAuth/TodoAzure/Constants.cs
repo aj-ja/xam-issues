c9547ae41f170ee8d8c29663fea903cd258c1301
Aija <ajaunteva@gmail.com>
2018-03-27 21:05:33 +0300

    initial commit

---
+++ Issue397_TodoAzureAuth/TodoAzure/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoAzure
+{
+    public static class Constants
+    {
+        // Replace string with your mobile service URL.
+        public static string ApplicationURL = @"MOBILE_SERVICE_URL";
+
+        public static string URLScheme = "issue397";
+    }
+}
+
---
+++ Issue397_TodoAzureAuth/iOS/iOSGoogleLogoutHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Foundation;
+using SafariServices;
+using UIKit;
+
+namespace TodoAzure.iOS
+{
+    public class iOSGoogleLogoutHelper : SFSafariViewControllerDelegate
+    {
+        public static void Logout(UIViewController controller)
+        {
+            var url = new NSUrl("https://www.google.com/accounts/Logout");
+            var safari = new SFSafariViewController(url, false);
+            safari.Delegate = new iOSGoogleLogoutHelper();
+            controller.PresentViewController(safari, true, null);
+        }
+
+        public override void DidCompleteInitialLoad(SFSafariViewController controller, bool didLoadSuccessfully)
+        {
+            controller.DismissViewControllerAsync(true);
+        }
+    }
+}
